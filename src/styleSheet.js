d70cc45acf593ef665136e03eac4daa5c4495a0f
Alastair Taft <ali.c.taft@gmail.com>
2017-03-06 11:36:09 +1100

    Added a addRules method to the stylesheet object.

--- src/styleSheet.js
+++ src/styleSheet.js
@@ -13,10 +13,26 @@ export function createStyleSheet(
     name,
     options,
     createRules,
+    addRules,
+  };
+
+  const additionalRules = []
+
+  function addRules(newRules){
+    additionalRules.push(newRules)
   };
 
   function createRules(theme: Object = {}): Object {
     const rules = typeof callback === 'function' ? callback(theme) : callback;
+
+    additionalRules.forEach(newRules => {
+      Object.keys(newRules).forEach(ruleName => {
+        if (rules[ruleName])
+          Object.assign(rules[ruleName], newRules[ruleName])
+        else
+          rules[ruleName] = newRules[ruleName]
+      })
+    })
 
     if (!theme.overrides || !theme.overrides[name]) {
       return rules;
